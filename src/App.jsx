91edeea0adf162fbd613dddeef0cdc0a033010e1
USMAN-PRO-TECH <usmannaeem52666@gmail.com>
2024-08-13 16:35:53 +0500

    background fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Game from './components/Game';
 
 function App() {
   return (
-    <div className='bg-main-bg bg-cover bg-center' >
+    <div className='bg-main-bg bg-cover bg-center'  style={{ backgroundImage: `url('./src/assets/background/bg.png')` }} >
       <Game />
     </div>
   );
